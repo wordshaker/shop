aa9bd0181bff5e8b0ed1a311f391b54e18b922dd
Jessica White <code.jessw@gmail.com>
2017-09-17 16:25:24 +0100

    returns static list of products

--- server.js
+++ server.js
@@ -7,9 +7,34 @@ start();
 function start(){
     var app = express();
     var server = http.Server(app);
+    var products = [{
+            name:"Isla Bra",
+            sku:"LN332",
+            price:29.00
+        },{
+            name:"Nordic Rose Bra",
+            sku:"LN336",
+            price:30.00
+        },{
+            name:"Zentangle Bra",
+            sku:"FY240",
+            price:34.00
+        },{
+            name:"Clara Bra",
+            sku:"PN112",
+            price:32.00
+        },{
+            name:"Deco Delight Bra",
+            sku:"FY158",
+            price:34.00
+        },{
+            name:"Sienna Bra",
+            sku:"LN328",
+            price:32.00
+        }];
 
     app.use('/products', (req, res, next) => {
-        res.status(200).send();
+        res.status(200).send(products);
     })
 
     server.listen(port, process.env.IP, () => {
