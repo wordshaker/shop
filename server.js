11f544f8a651de1194c76f0ce1ad40eb3b7beb7d
Jessica White <code.jessw@gmail.com>
2017-09-17 16:36:28 +0100

    refactor api to use router, sit under api path and add bag GET

--- server.js
+++ server.js
@@ -7,37 +7,45 @@ start();
 function start(){
     var app = express();
     var server = http.Server(app);
+    var router = express.Router();
+
     var products = [{
-            name:"Isla Bra",
-            sku:"LN332",
+            name:'Isla Bra',
+            sku:'LN332',
             price:29.00
         },{
-            name:"Nordic Rose Bra",
-            sku:"LN336",
+            name:'Nordic Rose Bra',
+            sku:'LN336',
             price:30.00
         },{
-            name:"Zentangle Bra",
-            sku:"FY240",
+            name:'Zentangle Bra',
+            sku:'FY240',
             price:34.00
         },{
-            name:"Clara Bra",
-            sku:"PN112",
+            name:'Clara Bra',
+            sku:'PN112',
             price:32.00
         },{
-            name:"Deco Delight Bra",
-            sku:"FY158",
+            name:'Deco Delight Bra',
+            sku:'FY158',
             price:34.00
         },{
-            name:"Sienna Bra",
-            sku:"LN328",
+            name:'Sienna Bra',
+            sku:'LN328',
             price:32.00
         }];
+    router.get('/products', (req, res) => {
+        res.json(products);
+    });
 
-    app.use('/products', (req, res, next) => {
-        res.status(200).send(products);
-    })
+    var bag = [];
+    router.get('/bag', (req, res) => {
+        res.json(bag);
+    });
+
+    app.use('/api', router);
 
     server.listen(port, process.env.IP, () => {
-        console.log("RESTFUL API STARTED ON " + port);
+        console.log('RESTFUL API STARTED ON ' + port);
     });
 }
